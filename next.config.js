def1236a3f085320f6b68b24d0c995954f45c07d
John Aughey <jha@aughey.com>
2025-03-14 17:55:46 -0500

    foo

--- next.config.js
+++ next.config.js
@@ -7,7 +7,14 @@ const nextConfig = {
     experimental: {
         // Enable client-side rendering by default
         appDir: true,
-    }
+    },
+    output: 'export',  // Enable static exports
+    images: {
+        unoptimized: true, // Required for static export
+    },
+    // Required for GitHub Pages - set the base path to your repo name
+    // You'll need to replace 'iitsec2' with your actual repository name
+    basePath: '/iitsec2',
 }
 
 module.exports = nextConfig 
